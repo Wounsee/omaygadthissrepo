8003899e4dc1c34f95e1c42cd19be4719be46efb
Wounsee <570artem@gmail.com>
2026-04-02 12:17:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10819993
+    "zoneId": 10820040
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
